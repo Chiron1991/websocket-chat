95f77499094f2a36bde7378dbd0d7cbd12ba6354
Chiron1991 <mail@chiron-online.de>
2017-04-27 01:09:33 +0200

    fix typo; fix wrong usage of .append()

--- client/main.js
+++ client/main.js
@@ -1,13 +1,15 @@
 $(function () {
     const socket = io();
-    const $messages = $("messages");
+    const $messages = $("#messages");
     const $messageInput = $("#m");
+
     $("form").submit(function () {
         socket.emit("chat message", $messageInput.val());
         $messageInput.val("");
         return false;
     });
+
     socket.on("chat message", function (msg) {
-        $messages.append($("li").text(msg));
+        $messages.append("<li>" + msg + "</li>");
     });
 });
